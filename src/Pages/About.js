5fe6399887b6f8d12f0b5aa8ec10bda62e31db4c
Abhi <abhij2116@gmail.com>
2022-11-28 15:51:45 +0530

    Routing Implemented

---
+++ src/Pages/About.js
@@ -0,0 +1,16 @@
+import Base from "../component/Base";
+
+const About=()=>{
+    return (
+        <Base>
+        <h1>This is About page.</h1>
+        <p>We are building blog WebSite.</p>
+        </Base>
+    )
+}
+
+export default About;
+
+
+
+
